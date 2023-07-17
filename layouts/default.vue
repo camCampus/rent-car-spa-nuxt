9e2a264fc0270276ed39016d2ab6d9edca89bd4e
camCampus <camille.bochard@le-campus-numerique.fr>
2023-07-17 14:43:47 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtLink to="/">
+      home
+    </NuxtLink>
+    <NuxtLink to="/about">
+      About page
+    </NuxtLink>
+    <slot/>
+  </div>
+</template>
+<script setup>
+</script>
