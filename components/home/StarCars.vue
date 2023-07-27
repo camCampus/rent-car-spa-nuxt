7aa1e5073b35ec12e6484d44c34eb0e1e0e3d5d9
Kize <bochu.nicolas@hotmail.fr>
2023-07-27 10:17:03 +0200

    update form v4

--- components/home/StarCars.vue
+++ components/home/StarCars.vue
@@ -10,14 +10,11 @@ export default defineComponent({
   <section class="py-10">
     <div class="container mx-auto">
       <h2 class="text-2xl font-bold m-2 mb-8">Voitures en vedette</h2>
-
       <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
         <!-- Remplacez les blocs ci-dessous par les détails réels de vos voitures en vedette -->
-
         <home-card-star-cars :cars-url="'/208.png'"/>
         <home-card-star-cars :cars-url="'/CLIO.png'"/>
         <home-card-star-cars :cars-url="'/CLIO.png'"/>
-
       </div>
     </div>
   </section>
