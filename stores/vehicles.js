c08eec847bdbe9acbe2de93fe9713443c0e1942d
Kize <bochu.nicolas@hotmail.fr>
2023-07-28 14:02:43 +0200

    many little update

--- stores/vehicles.js
+++ stores/vehicles.js
@@ -4,10 +4,14 @@ export const useVehicleStore = defineStore('vehicles', {
     state: () => ({
         baseUrl:'http://172.10.230.20:8082',
         priceList:[],
+        vehiclesList: [],
     }),
     actions: {
         async getVehiclesBasePrice() {
             this.vehicleList = await vehicleRepository.getVehiclesTypes();
         },
+        async getAllVehicles() {
+            this.vehiclesList = await vehicleRepository.getAllVehicules();
+        }
     }
 })
